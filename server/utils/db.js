b40820bb40f36a0a463e840220397deadd836110
Arin Melikyan <arin.melkn@gmail.com>
2023-03-03 01:58:37 +0400

    add db host

--- server/utils/db.js
+++ server/utils/db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.MYSQL_USER,
   process.env.MYSQL_PASS,
   {
-    host: 'mysqldb',
+    host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     dialect: 'mysql',
   }
